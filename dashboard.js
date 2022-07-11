441b02dba25b2c98be18c437a7df9e60d8303943
KamalAdityaM <80682678+KamalAdityaM@users.noreply.github.com>
2022-07-11 21:30:04 +0530

    Add files via upload

---
+++ dashboard.js
@@ -0,0 +1,31 @@
+$(".fa-comment").click(function(){
+  $("#theMenu").addClass("visible");
+});
+
+$(".fa-xmark").click(function(){
+  $("#theMenu").removeClass("visible");
+});
+
+$(".fa-video").click(function(){
+  $("#theStoriesMenu").addClass("visible");
+  $("#hideNavbar").addClass("visible");
+  $("#reactionsmenu").addClass("visible");
+});
+
+$(".fa-arrow-left").click(function(){
+  $("#theStoriesMenu").removeClass("visible");
+  $("#hideNavbar").removeClass("visible");
+  $("#reactionsmenu").removeClass("visible");
+});
+
+document.onkeydown = checkKey;
+
+function checkKey(e) {
+    e = e || window.event;
+    if (e.keyCode == '37') {
+       $("#prev")[0].click();
+    }
+    else if (e.keyCode == '39') {
+       $("#next")[0].click();
+    }
+}
